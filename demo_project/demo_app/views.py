d6ec9d105d07eac47709264c0fc599b00aae78b4
dadpenguin <franklinbula.100@gmail.com>
2024-04-19 01:48:57 +1200

    made penguin

--- demo_project/demo_app/views.py
+++ demo_project/demo_app/views.py
@@ -1,9 +1,56 @@
 from django.shortcuts import render, HttpResponse
 import random
+import json
 # Create your views here.
 
 def random_int(requests):
-    random_integer = random.randint(0, 100)
-    return HttpResponse(f"random Integer: {random_integer}")
+    penguin = {"penguin": []}
+
+    pengy = {
+        "name": "Pengy",
+        "species": "Emperor Penguin",
+        "age": 5,
+        "sex": "Male",
+        "location": "Antarctica",
+        "favorite_food": "Fish",
+        "weight_kg": 35,
+        "height_cm": 122
+    }
+    chilly = {
+        "name": "Chilly",
+        "species": "Adélie Penguin",
+        "age": 3,
+        "sex": "Female",
+        "location": "Antarctica",
+        "favorite_food": "Krill",
+        "weight_kg": 4,
+        "height_cm": 46
+    }
+    fluffy = {
+        "name": "Fluffy",
+        "species": "Gentoo Penguin",
+        "age": 2,
+        "sex": "Male",
+        "location": "Antarctica",
+        "favorite_food": "Squid",
+        "weight_kg": 5,
+        "height_cm": 51
+    }
+    waddle = {
+        "name": "Waddle",
+        "species": "Rockhopper Penguin",
+        "age": 4,
+        "sex": "Female",
+        "location": "Antarctica",
+        "favorite_food": "Crustaceans",
+        "weight_kg": 3.5,
+        "height_cm": 52
+    }
+
+    penguins = [pengy, chilly, fluffy, waddle]
+    random_penguin = random.choice(penguins)
+    penguin["penguin"].append(random_penguin)
+
+    return HttpResponse(json.dumps(penguin))
 
 
